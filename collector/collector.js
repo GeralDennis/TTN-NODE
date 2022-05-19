4f477d56eef2963e0b47c5fb9409fb859d65249e
GeralDennis <50561312+GeralDennis@users.noreply.github.com>
2022-05-19 08:11:34 -0500

    Update collector.js

--- collector/collector.js
+++ collector/collector.js
@@ -5,53 +5,15 @@
 const { insertDocument } = require('./mongo.js');
 var express = require("express");
 
-// MQTT setup
-var mqtt = require('mqtt');
-var options = {
-    port: 1883,
-    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username: '	lorawan-pico@ttn',
-    password: 'NNSXS.RYEECZRKJKHANK5EZN3SK23373IUQWLRML23AYY.FFTSCJ7JBUDVPNEZTKIBB6RCXEQNVEG3JCQHHER4UPJ2ALVZOHGQ',
-    keepalive: 60,
-    reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clean: true,
-    encoding: 'utf8'
-};
-var client = mqtt.connect('https://nam1.cloud.thethings.network',options);
-
-// Global variable to save data
-//var globalMQTT = 0;
-
-// MQTT setup
-client.on('connect', function() {
-    console.log('Client connected to TTN')
-    client.subscribe('#')
-});
-
-client.on('error', function(err) {
-    console.log(err);
-});
-
-client.on('message', function(topic, message) {
-    var getDataFromTTN = JSON.parse(message);
-    console.log("Data from TTN: ", getDataFromTTN.uplink_message.decoded_payload);
-    var messageObject = getDataFromTTN.uplink_message.decoded_payload;
-    //globalMQTT = Buffer.from(getFrmPayload, 'base64').toString();
-    insertDocument(messageObject)
-});
+var messageObject = {
+        "EC_soil": 1500,
+        "PH_soil": 7,
+        "env_humidity": 50,
+        "env_temperature": 24.1,
+        "mois_soil": 2,
+        "temp_soil": 30.1
+      }
+insertDocument(messageObject)
 
 
-function exitHandler(options, err) {
-    if (err) {
-        console.error('Application exiting...', err);
-    }
-    process.exit();
-}
 
-process.on('exit', exitHandler.bind(null, { cleanup: true }));
-process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
-process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
-process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
